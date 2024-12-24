6e393b3a66f6b079ed845a40beca23a173ed48de
Divine Patrick <patrickdivine70@gmail.com>
2024-12-24 12:54:06 +0100

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
+  build: {
+    outDir: 'dist',
+  },
   server: {
     proxy: {
       '/api': {
@@ -12,6 +14,5 @@ export default defineConfig({
       },
     },
   },
-
   plugins: [react()],
 })
